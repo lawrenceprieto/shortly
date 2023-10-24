c61d163b8a656b7608c30ba6f6f76bef74a79337
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-24 12:33:09 +0800

    add herobanner component

---
+++ src/components/Herobanner.jsx
@@ -0,0 +1,19 @@
+import "../styles/herobanner.css"
+import illustration from "../assets/images/illustration-working.svg"
+
+function Herobanner() {
+    return (
+        <>
+            <div className="herobanner">
+                <img className="hero-img" src={illustration} alt="hero-image" />
+                <div className="hero-details">
+                    <h1 className="heading">More than just shorter links</h1>
+                    <p className="sub-heading">Build your brand's recognition and get detailed insights on how your links are performing</p>
+                    <button className="">Get Started</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Herobanner
