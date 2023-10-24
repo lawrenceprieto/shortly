63fc8a78b93649857babf116bee9427963758d9e
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-24 12:34:43 +0800

    add footer banner component

---
+++ src/components/FooterBanner.jsx
@@ -0,0 +1,14 @@
+import "../styles/footerbanner.css"
+
+function FooterBanner() {
+    return (
+        <>
+            <div className="footer-banner">
+                <h2 className="ft-heading">Boosts your links today</h2>
+                <button className="">Get Started</button>
+            </div>
+        </>
+    )
+}
+
+export default FooterBanner
