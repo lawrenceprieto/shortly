aea473ea7aaeadf4936c04db468626125805d413
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-24 20:30:36 +0800

    feat: add a div for resul list

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -5,7 +5,26 @@ function InputField() {
         <>
             <div className="input-field">
                 <label htmlFor="link">Shorten It!</label>
-                <input type="text" id="link"></input>
+                <input type="text" id="link" 
+                    placeholder="Shorten a link here...">
+                </input>
+            </div>
+            <div className="result-list">
+                <div className="result-field">
+                    <div className="input-text">https://www.frontendmentor.io</div>
+                    <div className="input-result">https://rel.ink/k4lKyk</div>
+                    <button>Copy</button>
+                </div>
+                <div className="result-field">
+                    <div className="input-text">https://www.frontendmentor.io</div>
+                    <div className="input-result">https://rel.ink/k4lKyk</div>
+                    <button>Copy</button>
+                </div>
+                <div className="result-field">
+                    <div className="input-text">https://www.frontendmentor.io</div>
+                    <div className="input-result">https://rel.ink/k4lKyk</div>
+                    <button>Copy</button>
+                </div>
             </div>
         </>
     )
