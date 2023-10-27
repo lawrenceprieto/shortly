aaa1554eb0bdd69b1a7d2b8603c199e3652588bd
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-27 17:41:52 +0800

    refactor: change the label to button

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,18 +1,45 @@
+import { useState } from "react";
 import "../styles/inputfield.css"
 
 function InputField() {
+
+    const [url, setUrl] = useState('');
+
+    function handlechange(e) {
+        setUrl(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(url);
+        if (!url) {
+            alert('enter a url');
+        }
+    }
+
     return (
         <>
             <div className="input-field-body">
                 <div className="input-field">
-                    <label htmlFor="link">Shorten It!</label>
-                    <input type="text" id="link" 
-                        placeholder="Shorten a link here...">
-                    </input>
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" id="link" 
+                            placeholder="Shorten a link here..."
+                            onChange={handlechange}
+                            value={url} >
+                        </input>
+                        <button type="submit" className="input-field-btn">Shorten It!</button>
+                    </form>
                 </div>
             </div>
             <div className="result-list">
-                <div className="result-field">
+                {/* <div className="result-field">
+                    <div className="input-text">{shortenLinks.longLink}</div>
+                    <div className="result-cont">
+                        <div className="input-result">{shortenLinks.shortLink}</div>
+                        <button>Copy</button>
+                    </div>
+                </div> */}
+                {/* <div className="result-field">
                     <div className="input-text">https://www.frontendmentor.io</div>
                     <div className="result-cont">
                         <div className="input-result">https://rel.ink/k4lKyk</div>
@@ -25,14 +52,7 @@ function InputField() {
                         <div className="input-result">https://rel.ink/k4lKyk</div>
                         <button>Copy</button>
                     </div>
-                </div>
-                <div className="result-field">
-                    <div className="input-text">https://www.frontendmentor.io</div>
-                    <div className="result-cont">
-                        <div className="input-result">https://rel.ink/k4lKyk</div>
-                        <button>Copy</button>
-                    </div>
-                </div>
+                </div> */}
             </div>
         </>
     )
